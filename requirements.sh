a645538204e246447b433cd755c294497d2afd58
not populated <akrause4@cirrus-login2.ib0.icexa.epcc.ed.ac.uk>
2021-03-15 12:36:50 +0000

    update requirements

--- requirements.sh
+++ requirements.sh
@@ -1,15 +1,6 @@
 #!/usr/bin/env bash
 
-conda install --yes lxml
-conda install --yes nltk
-conda install --yes pep8
-conda install --yes pylint
-#conda install --yes pycodestyle
-conda install --yes pytest
-conda install --yes PyYAML
-conda install --yes regex
-conda install --yes requests
-conda install --yes pathlib
+conda install --yes lxml nltk pep8 pylint pytest PyYAML regex requests pathlib
 conda install -c conda-forge spacy
 python -m spacy download en
 python -m spacy download en_core_web_lg
