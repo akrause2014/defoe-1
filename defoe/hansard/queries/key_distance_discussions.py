aec2c29f4074612039c4d2fed985c92432d63097
akrause2014 <a.krause@epcc.ed.ac.uk>
2021-07-12 11:21:54 +0100

    updates to hansard queries

--- defoe/hansard/queries/key_distance_discussions.py
+++ defoe/hansard/queries/key_distance_discussions.py
@@ -6,6 +6,7 @@ import yaml
 
 from defoe import query_utils
 from defoe.hansard.utils import find_matches, get_headings, clean_text
+from defoe.papers.query_utils import within_distance
 
 def do_query(hansards, config_file=None, logger=None, context=None):
     
@@ -15,6 +16,8 @@ def do_query(hansards, config_file=None, logger=None, context=None):
     preprocess_type = query_utils.extract_preprocess_word_type(config)
     print(f'preprocessing: {preprocess_type}')
 
+    distance = int(config['distance'])
+
     unproc_keywords = config['keywords']
     keywords = []
     for k in unproc_keywords:
@@ -22,6 +25,14 @@ def do_query(hansards, config_file=None, logger=None, context=None):
             [query_utils.preprocess_word(word, preprocess_type) for word in k.split()])
         )
     print(f'keywords: {keywords}')
+
+    unproc_controlwords = config['controlwords']
+    controlwords = []
+    for c in unproc_controlwords:
+        controlwords.append(' '.join(
+            [query_utils.preprocess_word(word, preprocess_type) for word in c.split()])
+        )
+    print(f'controlwords: {controlwords}')
 
     # [(year, discussion_string), ...]
     headings = hansards.flatMap(
@@ -41,9 +52,10 @@ def do_query(hansards, config_file=None, logger=None, context=None):
         lambda disc: any(k in disc[2] for k in keywords))
 
     # [(discussion, clean_text)
-    matching_discussions = filter_discussions.map(
-        lambda disc: (disc[0], disc[1], find_matches(disc[2], keywords)))
-
+    matching_discussions = filter_discussions.flatMap(
+        lambda disc: [
+            (disc[0], disc[1], w) for w in within_distance(keywords, controlwords, disc[2], distance)
+        ])
 
     matching_data = matching_discussions.flatMap(
         lambda discussion:
@@ -54,8 +66,10 @@ def do_query(hansards, config_file=None, logger=None, context=None):
                 "heading_id": discussion[1]._id,
                 "speech_id": speech._id,
                 "speaker": ((speech.speaker.id, speech.speaker.name) if speech.speaker is not None else ''),
-                "text": speech.text,                
+                "text": speech.text,
                 "filename": discussion[0].filename,
+                "keywords": list(discussion[2][1]),
+                "controls": list(discussion[2][0]),
             }
          )
          for speech in discussion[1].speeches
